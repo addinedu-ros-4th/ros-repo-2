d3df32a1bc0d9a8f06d895b5aed5ad4f32ed5b3c
AUTO-KKYU <dknjy3313@gmail.com>
2024-05-18 14:55:46 +0900

    Feat : ArUco detection package

---
+++ minibot_arucomarker/minibot_arucomarker/img_publisher.py
@@ -0,0 +1,45 @@
+import rclpy 
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge 
+import cv2
+
+class ImgPublisher(Node):
+    def __init__(self):
+        super().__init__('img_publisher')
+        self.publisher = self.create_publisher(
+                                    Image, 
+                                    '/camera', 
+                                    10)
+        time_period = 0.01
+        self.timer = self.create_timer(time_period, 
+                                    self.time_callback)
+        
+        self.cap = cv2.VideoCapture(0)
+        self.cv_bridge = CvBridge()
+
+        self.declare_parameter('width', 640)
+        self.width = self.get_parameter('width').value
+        self.declare_parameter('length', 480)
+        self.length = self.get_parameter('length').value
+
+        output_msg = "Video Width : " + str(self.width) + "\n\r"
+        output_msg = output_msg + "Video Length : " + str(self.length)
+        self.get_logger().info(output_msg)
+
+    def time_callback(self):
+        ret, frame = self.cap.read()
+        frame = cv2.resize(frame, (self.width, self.length))
+        img = self.cv_bridge.cv2_to_imgmsg(frame, "bgr8")
+        self.publisher.publish(img)
+        #cv2.waitKey(1)
+
+def main() :
+    rclpy.init()
+    node = ImgPublisher()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__' :
+    main()
---
+++ minibot_arucomarker/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'minibot_arucomarker'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='yongtak_ras',
+    maintainer_email='yongtak_ras@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'detect_aruco = minibot_arucomarker.detect_aruco:main',
+            'img_publisher =minibot_arucomarker.img_publisher:main',
+        ],
+    },
+)
