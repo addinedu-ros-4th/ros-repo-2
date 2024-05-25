c1bcfd0dc8673b8ff72c6e0471344147693fa4be
gaeun0123 <cheun0928@naver.com>
2024-05-25 15:57:54 +0900

    ♻️ Refactor: change database structer, query

--- db/query/create_table.sql
+++ db/query/create_table.sql
@@ -1,14 +1,14 @@
-CREATE TABLE IF NOT EXISTS ProductOrder (
-    user_id INT NOT NULL,
-    item_id INT NOT NULL,
-    items VARCHAR(16) NOT NULL,
-    quantities INT NOT NULL,
-    order_time TIMESTAMP NOT NULL
-
+CREATE TABLE IF NOT EXISTS ProductInventory (
+    item_id INT NOT NULL PRIMARY KEY,
+    item_name VARCHAR(16) NOT NULL,
+    stock INT NOT NULL DEFAULT 8
 );
 
-CREATE TABLE IF NOT EXISTS ProductInventory (
+CREATE TABLE IF NOT EXISTS ProductOrder (
+    order_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
     item_id INT NOT NULL,
-    items VARCHAR(16) NOT NULL,
-    stock INT NOT NULL DEFAULT 100
+    quantities INT NOT NULL,
+    order_time TIMESTAMP NOT NULL,
+    FOREIGN KEY (item_id) REFERENCES ProductInventory(item_id)
 );
