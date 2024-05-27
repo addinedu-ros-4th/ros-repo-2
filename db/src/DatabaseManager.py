f100df31bdb7dc709968578fc27c13a71632311e
HALYNNN <jean906@naver.com>
2024-05-27 17:11:46 +0900

    Set not to duplicate user id

--- db/src/DatabaseManager.py
+++ db/src/DatabaseManager.py
@@ -42,7 +42,6 @@ class DatabaseManager:
                 raise
         self.cur = self.conn.cursor()
     
-    
     def create_database(self, db_name):
         try:
             self.cur.execute(f"CREATE DATABASE {db_name}")
@@ -52,11 +51,9 @@ class DatabaseManager:
         print(f"Database {db_name} created successfully.")
         self.cur.execute(f"USE {db_name}")
     
-    
     def create_table(self):
         self.execute_sql_file("db/query/create_table.sql")
         self.initialize_inventory()
-    
     
     def execute_sql_file(self, file_path):
         with open(file_path, 'r') as file:
@@ -70,7 +67,6 @@ class DatabaseManager:
                 print(f"Error occurred: {err}")
         self.conn.commit()
     
-    
     def save_data(self, table_name, data):
         columns = ', '.join(data.keys())
         placeholders = ', '.join(['%s' for _ in data])
@@ -79,28 +75,24 @@ class DatabaseManager:
         self.conn.commit()
         self.cur.execute("SELECT LAST_INSERT_ID()")
         last_id = self.cur.fetchone()[0]
-        self.cur.fetchall()
+        self.cur.fetchall()  # 이전 쿼리의 결과를 모두 읽음
         return last_id
-
 
     def get_product_id(self, product_name):
         product_ids = {"cola": 1, "water": 2, "ramen": 3}
         return product_ids.get(product_name.lower(), None)
 
-
     def get_stock(self, item_id):
         query = "SELECT stock FROM ProductInventory WHERE item_id = %s"
         self.cur.execute(query, (item_id,))
         result = self.cur.fetchone()
-        self.cur.fetchall() 
+        self.cur.fetchall()  # 이전 쿼리의 결과를 모두 읽음
         return result[0] if result else None
-
 
     def update_stock(self, item_id, quantity):
         query = "UPDATE ProductInventory SET stock = stock - %s WHERE item_id = %s"
         self.cur.execute(query, (quantity, item_id))
         self.conn.commit()
-
 
     def initialize_inventory(self):
         inventory_data = [
@@ -109,16 +101,14 @@ class DatabaseManager:
             (3, "ramen", 100)
         ]
         for item_id, item_name, stock in inventory_data:
-            self.cur.execute("INSERT IGNORE INTO ProductInventory (item_id, items, stock) VALUES (%s, %s, %s)", (item_id, item_name, stock))
+            self.cur.execute("INSERT IGNORE INTO ProductInventory (item_id, item_name, stock) VALUES (%s, %s, %s)", (item_id, item_name, stock))
         self.conn.commit()
-
 
     def close_connection(self):
         if self.cur:
             self.cur.close()
         if self.conn:
             self.conn.close()
-    
     
     def find_elements(self, name, password):
         query = "SELECT UserId, Name, Password from Users where Name = %s and (Password) = %s;"
@@ -127,6 +117,13 @@ class DatabaseManager:
         self.cur.fetchall()  # 이전 쿼리의 결과를 모두 읽음
         self.close_connection()
         return result
+
+    def get_last_user_id(self):
+        query = "SELECT MAX(user_id) FROM ProductOrder"
+        self.cur.execute(query)
+        result = self.cur.fetchone()
+        self.cur.fetchall()  # 이전 쿼리의 결과를 모두 읽음
+        return result[0] if result[0] is not None else 0
 
 
 if __name__ == '__main__':
