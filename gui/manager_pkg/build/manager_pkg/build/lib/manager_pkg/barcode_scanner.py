0e874f52c97e01e2aad292c8206df59031f723a3
gaeun0123 <cheun0928@naver.com>
2024-06-07 16:34:56 +0900

    ♻️ Refactor: add inbound_id

--- gui/manager_pkg/build/manager_pkg/build/lib/manager_pkg/barcode_scanner.py
+++ gui/manager_pkg/build/manager_pkg/build/lib/manager_pkg/barcode_scanner.py
@@ -61,9 +61,10 @@ class BarcodeScanner(QObject):
                     self.db_manager.add_to_stock(item_id, quantity)
                     
                     # Change format to send to ros
-                    self.inbound_data["id"] = primary_key
+                    self.inbound_data["inbound_id"] = primary_key
                     del self.inbound_data["quantity"]
                     self.inbound_data["quantities"] = quantity
+                    
                     self.send_task_to_ros()
                     
                     self.barcode_scanned.emit(self.inbound_data)  # Emit signal with inbound data
